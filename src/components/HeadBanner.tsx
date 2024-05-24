d2fdc134b0d1c8a2e38a83f6c90a38bfe91370ad
genics85 <eugeneamo85@gmail.com>
2024-05-24 20:49:30 +0000

    next

--- src/components/HeadBanner.tsx
+++ src/components/HeadBanner.tsx
@@ -5,15 +5,15 @@ function HeadBanner() {
     <div className="hidden h-[44px] lg:flex items-center bg-[#1a1f2e] w-full">
       <div className=" container text-[20px] mx-auto px-4 flex justify-between w-full ">
         <div className="flex gap-2 scrolling-divs ">
-          <p className=" text-nowrap">I am a doogle</p>
+          <p className=" text-nowrap">I am DOOGLER</p>
           <img src={banner_star} alt="" />
         </div>
         <div className="flex gap-2 scrolling-divs">
-          <p className=" text-nowrap">I am a meme doogler</p>
+          <p className=" text-nowrap">I am MEME doogler</p>
           <img src={banner_star} alt="" />
         </div>
         <div className="flex gap-2 scrolling-divs ">
-          <p className=" text-nowrap">I am a meme doogleinu</p>
+          <p className=" text-nowrap">I am Doogle Inu.</p>
           <img src={banner_star} alt="" />
         </div>
         <div className="flex gap-2 scrolling-divs">
